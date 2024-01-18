6f61dadb7eda6fb3c59e103e76d76b673f5b8d53
Johannes Kantz <67144859+JohannesKantz@users.noreply.github.com>
2024-01-18 04:41:43 +0100

    rename file

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 
 # Initialize 
 # Start nginx and certbot to get the certificate
-docker-compose -f ./docker-compose-initiate.yaml up -d nginx
-docker-compose -f ./docker-compose-initiate.yaml up certbot
-docker-compose -f ./docker-compose-initiate.yaml down
+docker-compose -f ./docker-compose-initiate.yml up -d web
+docker-compose -f ./docker-compose-initiate.yml up certbot
+docker-compose -f ./docker-compose-initiate.yml down
 
 # generate the dhparam.pem file
 openssl dhparam -out ./etc/nginx/ssl/dhparam.pem 2048
 
 
 # Start the main docker-compose
-docker-compose -f ./docker-compose.yaml -d up
+docker-compose -f ./docker-compose.yml -d up
